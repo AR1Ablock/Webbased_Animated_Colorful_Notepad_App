d44bc699e46e5e4cf0c2f732c8980fcfabec3058
AR1Ablock <78879680+AR1Ablock@users.noreply.github.com>
2025-02-16 02:43:03 +0500

    Delete src directory

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-/* storage is a var and app.vue is stored inside it */
-import storage_variable from './App.vue'
-import './index.css'
-
-/* here it mount that storage var under .con class of html file */
-createApp(storage_variable).mount('.con');
